f1542e0cbf4a19dd93b2e7cbf91e5596567be20e
Andrew Rowley <andrew@internetdrew.com>
2023-01-19 13:29:18 -0500

    decifer google jwt web token

---
+++ frontend/src/components/GoogleLogin.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { useEffect, useState } from 'react';
+import jwt_decode from 'jwt-decode';
+
+const GoogleLogin = () => {
+  const [user, setUser] = useState({});
+
+  const handleCredentialResponse = response => {
+    console.log(`Encoded JWT ID token: ${response.credential}`);
+    const userObject = jwt_decode(response.credential);
+    console.log(userObject);
+    setUser(userObject);
+  };
+
+  useEffect(() => {
+    google.accounts.id.initialize({
+      client_id: import.meta.env.VITE_GOOGLE_CLIENT_ID,
+      callback: handleCredentialResponse,
+    });
+
+    google.accounts.id.renderButton(document.getElementById('signInDiv'), {
+      theme: 'outline',
+      size: 'large',
+    });
+
+    google.accounts.id.prompt();
+  }, []);
+
+  return <div id='signInDiv'></div>;
+};
+
+export default GoogleLogin;
--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -4,6 +4,7 @@ import { useNavigate } from 'react-router-dom';
 import { FcGoogle } from 'react-icons/fc';
 import shareVideo from '../assets/share.mp4';
 import logo from '../assets/logowhite.png';
+import GoogleLogin from './GoogleLogin';
 
 const Login = () => {
   useEffect(() => {
@@ -22,7 +23,14 @@ const Login = () => {
           className='w-full h-full object-cover'
         ></video>
 
-        <div className='absolute flex flex-col justify-center items-center top-0 right-0 left-0 bottom-0 bg-blackOverlay'></div>
+        <div className='absolute flex flex-col justify-center items-center top-0 right-0 left-0 bottom-0 bg-blackOverlay'>
+          <div className='p-5'>
+            <img src={logo} alt='logo' width='130px' />
+          </div>
+          <div className='shadow-2xl'>
+            <GoogleLogin />
+          </div>
+        </div>
       </div>
     </div>
   );

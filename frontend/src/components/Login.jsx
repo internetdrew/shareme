dc97cc8e86f6558c143769b5a512017f57536519
Andrew Rowley <andrew@internetdrew.com>
2023-01-19 12:13:28 -0500

    add video overlay

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -21,6 +21,8 @@ const Login = () => {
           autoPlay
           className='w-full h-full object-cover'
         ></video>
+
+        <div className='absolute flex flex-col justify-center items-center top-0 right-0 left-0 bottom-0 bg-blackOverlay'></div>
       </div>
     </div>
   );

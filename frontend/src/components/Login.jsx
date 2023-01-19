415e812c880f49adf10e1367081c876c316d6dbe
Andrew Rowley <andrew@internetdrew.com>
2023-01-19 12:09:02 -0500

    make video full screen for login

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -2,12 +2,28 @@ import React from 'react';
 import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { FcGoogle } from 'react-icons/fc';
+import shareVideo from '../assets/share.mp4';
+import logo from '../assets/logowhite.png';
 
 const Login = () => {
   useEffect(() => {
     console.log(import.meta.env.VITE_GOOGLE_CLIENT_ID);
   }, []);
-  return <div>Login</div>;
+  return (
+    <div className='flex justify-start items-center flex-col h-screen'>
+      <div className='relative w-full h-full'>
+        <video
+          src={shareVideo}
+          type='video/mp4'
+          loop
+          controls={false}
+          muted
+          autoPlay
+          className='w-full h-full object-cover'
+        ></video>
+      </div>
+    </div>
+  );
 };
 
 export default Login;

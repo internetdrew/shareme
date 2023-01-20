949373cc48353bf3e17c2bad6c1e68f026f5fb71
Andrew Rowley <andrew@internetdrew.com>
2023-01-20 13:02:33 -0500

    remove useNavigate import

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
 import { FcGoogle } from 'react-icons/fc';
 import shareVideo from '../assets/share.mp4';
 import logo from '../assets/logowhite.png';

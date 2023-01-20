be9f59e4b7bf7533a1b2546f905046cb41a2bd7b
Andrew Rowley <andrew@internetdrew.com>
2023-01-20 13:59:27 -0500

    add pins container

--- frontend/src/container/Home.jsx
+++ frontend/src/container/Home.jsx
@@ -2,6 +2,8 @@ import React, { useState, useRef, useEffect } from 'react';
 import { HiMenu } from 'react-icons/hi';
 import { Link, Route, Routes } from 'react-router-dom';
 import { Sidebar, UserProfile } from '../components';
+import { client } from '../client';
+import logo from '../assets/logo.png';
 
 const Home = () => {
   return <div>Home</div>;

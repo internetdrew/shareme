39e9064e9704d5a4d3cb3123d1fa08d47a1955a6
Andrew Rowley <andrew@internetdrew.com>
2023-01-21 11:36:27 -0500

    import pins container components

--- frontend/src/container/Pins.jsx
+++ frontend/src/container/Pins.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import { Routes, Route } from 'react-router-dom';
 
+import { CreatePin, Feed, Navbar, Search, PinDetail } from '../components';
+
 const Pins = () => {
   return <div>Pins</div>;
 };

7b88f015a210bce2793ed63f0baae7126687c000
Andrew Rowley <andrew@internetdrew.com>
2023-01-21 14:21:04 -0500

    remove duplicate avatar on mobile

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -9,16 +9,25 @@ const Navbar = ({ searchTerm, setSearchTerm, user }) => {
 
   return (
     <div className='flex gap-2 md:gap-5 w-full mt-5 pb-7'>
-      <div className='flex justify-start items-center w-full p-2 rounded-md bg-white border-none outline-none focus-within:shadow-sm'>
+      <div className='flex items-center w-full p-2 rounded-md bg-white border-none outline-none focus-within:shadow-sm'>
         <IoMdSearch fontSize={21} className='ml-1' />
         <input
           type='text'
-          className='w-1/3 outline-none ml-1'
+          className='w-1/3 outline-none bg-white ml-2 p-1'
           onChange={e => setSearchTerm(e.target.value)}
           placeholder='Search'
           value={searchTerm}
           onFocus={() => navigate('/search')}
         />
+      </div>
+      <div className='flex gap-3'>
+        <Link>
+          <img
+            src={user.image}
+            alt='user'
+            className='hidden w-12 hg-12 rounded-full md:inline-block'
+          />
+        </Link>
       </div>
     </div>
   );

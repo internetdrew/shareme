3be9de134124abc1b7aee9eb82253dffe4039c3d
Andrew Rowley <andrew@internetdrew.com>
2023-01-21 15:06:46 -0500

    add final touches to navbar component

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = ({ searchTerm, setSearchTerm, user }) => {
   if (!user) return null;
 
   return (
-    <div className='flex gap-2 md:gap-5 w-full mt-5 pb-8'>
+    <div className='flex gap-2 md:gap-5 w-full mt-5 pb-8 items-center'>
       <div className='flex p-2 rounded-md bg-white items-center border-none outline-none focus-within:shadow-sm grow'>
         <IoMdSearch fontSize={21} className='ml-1' />
         <input
@@ -22,11 +22,11 @@ const Navbar = ({ searchTerm, setSearchTerm, user }) => {
       </div>
       <div className='flex items-center justify-end gap-2'>
         <Link to={`user-profile/${user?._id}`} className='hidden md:block'>
-          <img src={user.image} alt='user' className='w-12 rounded-full' />
+          <img src={user.image} alt='user' className='w-10 rounded-full' />
         </Link>
         <Link
           to='/create-pin'
-          className='bg-black text-white flex justify-center items-center w-12 h-12 rounded-full'
+          className='bg-black text-white flex justify-center items-center w-10 h-10 rounded-full'
         >
           <IoMdAdd fontSize={20} />
         </Link>

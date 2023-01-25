42df9685e75cdb558cd8daea68f35adba171300f
Andrew Rowley <andrew@internetdrew.com>
2023-01-24 20:17:16 -0500

    fix broken create pins route

--- frontend/src/container/Pins.jsx
+++ frontend/src/container/Pins.jsx
@@ -23,7 +23,7 @@ const Pins = ({ user }) => {
             path='/pin-detail/:pinId'
             element={<PinDetail user={user} />}
           />
-          <Route path='/createPin' element={<CreatePin user={user} />} />
+          <Route path='/create-pin' element={<CreatePin user={user} />} />
           <Route
             path='/search'
             element={

83e51b13f437c2e94071bd61048bf1180f8a52d5
Andrew Rowley <andrew@internetdrew.com>
2023-01-27 23:08:43 -0500

    add new database queries

--- frontend/src/components/UserProfile.jsx
+++ frontend/src/components/UserProfile.jsx
@@ -1,6 +1,38 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { AiOutlineLogout } from 'react-icons/ai';
+import { useParams, useNavigate } from 'react-router-dom';
+
+import {
+  userCreatedPinsQuery,
+  userQuery,
+  userSavedPinsQuery,
+} from '../utils/data';
+import { client } from '../client';
+import MasonryLayout from 'react-masonry-css';
+import Spinner from './Spinner';
 
 const UserProfile = () => {
+  const [user, setUser] = useState(null);
+  const [pins, setPins] = useState(null);
+  const [text, setText] = useState('Created');
+  const [activeBtn, setActiveBtn] = useState('created');
+  const navigate = useNavigate();
+  const { userId } = useParams();
+
+  // if (!user) {
+  //   return <Spinner message='Loading profile...' />;
+  // }
+
+  useEffect(() => {
+    console.log('here');
+    const query = userQuery(userId);
+
+    client.fetch(query).then(data => {
+      setUser(data[0]);
+      console.log(user);
+    });
+  }, [userId]);
+
   return <div>UserProfile</div>;
 };
 

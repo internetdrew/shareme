36ca3dbe17cba057266009d0fcc195a6316e7cd8
Andrew Rowley <andrew@internetdrew.com>
2023-01-28 00:37:10 -0500

    add randomly generated urls for user profile header

--- frontend/src/components/UserProfile.jsx
+++ frontend/src/components/UserProfile.jsx
@@ -10,6 +10,7 @@ import {
 import { client } from '../client';
 import MasonryLayout from 'react-masonry-css';
 import Spinner from './Spinner';
+import { createApi } from 'unsplash-js';
 
 const UserProfile = () => {
   const [user, setUser] = useState(null);
@@ -18,16 +19,32 @@ const UserProfile = () => {
   const [activeBtn, setActiveBtn] = useState('created');
   const navigate = useNavigate();
   const { userId } = useParams();
+  const [randomImageUrl, setRandomImageUrl] = useState('');
 
-  const randomImage =
-    'https://source.unsplash.com/1600*900/?nature,photography,technology';
+  useEffect(() => {
+    const getRandomImage = async () => {
+      const res = await fetch(
+        `https://api.unsplash.com/photos/?client_id=${
+          import.meta.env.VITE_UNSPLASH_ACCESS_KEY
+        }`
+      );
+      const randomImages = await res.json();
+      const extractedUrls = randomImages.map(
+        image => `${image.urls.regular}&w=1600&h=900`
+      );
+      const randomUrl =
+        extractedUrls[Math.floor(Math.random() * extractedUrls.length)];
+      setRandomImageUrl(randomUrl);
+    };
+
+    getRandomImage();
+  }, []);
 
   useEffect(() => {
     const query = userQuery(userId);
 
     client.fetch(query).then(data => {
       setUser(data[0]);
-      console.log(user);
     });
   }, [userId]);
 
@@ -40,7 +57,11 @@ const UserProfile = () => {
       <div className='flex flex-col pb-5'>
         <div className='relative flex flex-col mb-7'>
           <div className='flex flex-col justify-center items-center'>
-            <img src={randomImage} alt='' className='w-full h-370 2xl:h' />
+            <img
+              src={randomImageUrl}
+              alt='banner-pic'
+              className='w-full h-370 2xl:h-510 shadow-lg object-cover'
+            />
           </div>
         </div>
       </div>
